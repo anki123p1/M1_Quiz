eb00b4469f3e4063b4fb0ecda24188eda8dd0530
anki123p1 <101923433+anki123p1@users.noreply.github.com>
2022-03-31 20:13:30 +0530

    Add files via upload

---
+++ 3_Implementation/src/score.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+#include<windows.h>
+#include<ctype.h>
+#include<stdlib.h>
+#include<time.h>
+#include<direct.h>
+#include"main.h"
+
+void writescore(float points,char plnm[20])
+ {
+ float sc;
+ char nm[20];
+ FILE *f;
+ system("cls");
+ f=fopen("points.txt","r");
+ fscanf(f,"%s%f",&nm,&sc);
+ if (points>=sc)
+   { sc=points;
+     fclose(f);
+     f=fopen("points.txt","w");
+     fprintf(f,"%s\n%.2f",plnm,sc);
+     fclose(f);
+   }
+ }
